6b41223f0c60cc17977a38891f17d136be6dc4f4
ayushree19 <ayushreeshristava2007@gmail.com>
2026-09-18 09:31:26 +0530

    Add manual CORS headers

--- server.js
+++ server.js
@@ -17,7 +17,16 @@ const corsOptions = {
     credentials: true
 };
 
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
 app.use(express.json());
 app.use(express.static(__dirname));
 
